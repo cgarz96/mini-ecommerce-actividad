a2c7c45ac4ad0d23d48fb7d0deac7bf4e1c1c8e8
cgarz96 <cgarzon388@gmail.com>
2021-12-30 19:44:25 -0300

    Agregado de navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,27 @@
 import React from "react";
 import { Row,Col } from "react-bootstrap";
+import { Navbar, Container } from "react-bootstrap";
+import logo from '../logo.svg'
+
+const style = {
+    marginBottom: '30px'
+  } 
 
 export const Header = () => {
     return (
-        <Row>
-            <Col md={12}>
-                <h1>Mini ecommerce</h1>
-            </Col>
-        </Row>
+        <Navbar bg="dark" variant="dark" style={style}>
+            <Container>
+                <Navbar.Brand href="#home">
+                    <img
+                    alt=""
+                    src={logo}
+                    width="30"
+                    height="30"
+                    className="d-inline-block align-top"
+                    />{' '}
+                Mini Ecommerce
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
     )
 }
