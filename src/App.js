37ebc382183faeca59c8bc7a467aa749e87774bc
cgarz96 <cgarzon388@gmail.com>
2021-12-30 19:29:17 -0300

    Uso de session storage

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import {Header} from "./components/Header";
 import {ListProducts} from './components/Products/ListProducts'
+import { AlertProduct } from "./components/Products/AlertProducts";
 
 
 function App() {
   return (
     <Container >
       <Header/>
+      <AlertProduct />
       <ListProducts/>
     </Container>
   );
--- src/components/Products/Product.js
+++ src/components/Products/Product.js
@@ -1,5 +1,6 @@
 import { Col, Row, Button, Badge} from "react-bootstrap";
 import "../../assets/css/styles.css";
+import { saveLastInterestProduct } from "../../app/services/storageServices";
 export const Product = ({item}) => {
     const {title,price,image,description}= item
     return(
@@ -10,7 +11,7 @@ export const Product = ({item}) => {
             <div >{title}</div>
         </Col>
         <Col xs={4}>
-            <Button variant="primary">
+            <Button variant="primary" onClick={() => saveLastInterestProduct(title) }>
             Precio <Badge bg="info">{price}</Badge>
             <span className="visually-hidden">$</span>
             </Button>
@@ -22,6 +23,14 @@ export const Product = ({item}) => {
         </Col>
         <Col xs={6}>
             <div>{description.slice(0,70)}...</div>
+        </Col>
+        </Row>
+        <Row>
+        <Col xs={12}>
+            <Button variant="warning" className="m-1" >
+             <Badge bg="secondary">🛒Agregar al carrito</Badge>
+            <span className="visually-hidden"> </span>
+            </Button>
         </Col>
         </Row>
         </Col>
