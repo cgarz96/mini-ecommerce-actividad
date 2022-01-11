c3a5918691603b3b8938d0158f09d823d86cf209
cgarz96 <cgarzon388@gmail.com>
2022-01-10 23:25:50 -0300

    Creando ruta dinamica

--- src/components/ShoppingCart/ShoppingCartItem.js
+++ src/components/ShoppingCart/ShoppingCartItem.js
@@ -3,5 +3,5 @@ import { NavDropdown } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export const ShoppingCartItem = ({item}) => (
-  <NavDropdown.Item><Link to={`/product/${item.id}`}>{item.title} - {item.category} - ${item.price}</Link> </NavDropdown.Item>
+  <NavDropdown.Item><Link to={`/product/${item.id}`}>{item.title} - {item.category} - ${item.price} </Link> </NavDropdown.Item>
 );
