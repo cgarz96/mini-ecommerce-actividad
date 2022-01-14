bcaea2378997c1a33ab1233474d7710f87c55c0d
cgarz96 <cgarzon388@gmail.com>
2022-01-14 19:36:12 -0300

    Listado de detalles de productos del carrito de compras

--- src/app/services/productServices.js
+++ src/app/services/productServices.js
@@ -4,3 +4,8 @@ export const getProductById = async (id) => {
     const productFromDB = await db.cart.get(Number(id));
     return productFromDB;
 }
+
+export const getAllProducts = async () => {
+    const allProductsFromDB = await db.cart.toArray();
+    return allProductsFromDB;
+}
--- src/views/Purchase.js
+++ src/views/Purchase.js
@@ -1,8 +1,21 @@
-
-export const Purchase = () =>{
+import { DetailCart } from '../components/ShoppingCart/DetailCart';
+import { Button, Col, Row } from 'react-bootstrap';
+export const Purchase = ({history}) =>{
+    const handleCancelButton = () => {
+        history.push('/')
+    }
     return (
         <>
-            <h1>Pruchase</h1>
+            <h1>Detalles de tu carrito de compras</h1>
+            <DetailCart />
+            <Row>
+                <Col xs={6}>
+                    <Button onClick={handleCancelButton} variant='danger'>Cancelar</Button>
+                </Col>
+                <Col xs={6}>
+                    <Button variant='success'>Comprar</Button>
+                </Col>
+            </Row>
         </>
     )
 }
